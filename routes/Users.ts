86e25db57a6a6c2cbc73c2cb0959533a42f30f14
Marcelo Coronel-Linux <jmcoronelr19@gmail.com>
2024-02-01 01:43:44 -0300

    .

--- routes/Users.ts
+++ routes/Users.ts
@@ -1,71 +1,29 @@
-import { Router, Request, Response } from "express";
-import { User } from "../models/User";
-
+import { Router} from "express";
+import * as controller from "../Controllers/UserController";
 const router = Router();
-let users: User[] = [];
-
-//Create new user
-router.post("/new", (req: Request, res: Response) => {
-  const user: User = {
-    id: users.length + 1,
-    ci: req.body.ci,
-    name: req.body.name,
-    lastName: req.body.lastName,
-    phone: req.body.phone,
-    email: req.body.email,
-    password: req.body.password,
-    house_number: req.body.house_number,
-    bills: [],
-    assists: [],
-  };
-  users.push(user);
-  res.status(201).json(user);
-});
 
 //Get all users
 
-router.get("/", (req: Request, res: Response) => {
-  res.json(users);
-});
+router.get("/", controller.getUsers);
 
 //Get user by id
 
-router.get("/:id", (req: Request, res: Response) => {
-  const user = users.find((u) => u.id === parseInt(req.params.id));
+router.get("/:id", controller.getUserById);
 
-  if (!user) {
-    res.status(404).send("User not found!");
-  }
-});
+//Create new user
 
-// Update user personal information
-
-router.put("/:id/info", (req: Request, res: Response) => {
-  const user = users.find((u) => u.id === parseInt(req.params.id));
-
-  if (!user) {
-    res.status(404).send("User not found!");
-  } else {
-    user.name = req.body.name || user.name;
-    user.lastName = req.body.lastName || user.lastName;
-    user.email = req.body.email || user.email;
-    user.phone = req.body.phone || user.phone;
-    user.ci = req.body.ci || user.ci;
-
-    res.json(user);
-  }
-});
+router.post("/", controller.createUser);
 
 // Delete user
 
-router.delete("/:id", (req: Request, res: Response) => {
-  const index = users.findIndex((u) => u.id === parseInt(req.params.id));
+router.delete("/:id", controller.deleteUser);
 
-  if (index === -1) {
-    res.status(404).send("User not found!");
-  } else {
-    users.slice(index, 1);
-    res.status(204).send();
-  }
-});
+// Update user personal information
+
+router.put("/:id",controller.updateUserInfo);
+
+// Update user password
+
+router.put("/:id/password", controller.updateUserPass);
+
 export default router;
