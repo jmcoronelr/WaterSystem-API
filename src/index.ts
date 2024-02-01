a9cd5ced918907b9879dcae6cc897a0652bd2495
Marcelo Coronel-Linux <jmcoronelr19@gmail.com>
2024-02-01 01:44:01 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import userRoutes from '../routes/Users';
+import billRoutes from '../routes/Bills';
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/users', userRoutes);
+app.use('/users/:id/bills', billRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Main Page !");
