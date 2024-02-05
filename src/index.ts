412f6cedce4d851dca02e2c8ad86eadd37949943
Marcelo Coronel-Linux <jmcoronelr19@gmail.com>
2024-02-05 12:04:54 -0300

    Routes added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express, { Application, Request, Response } from "express";
-import userRoutes from '../routes/Users';
-import billRoutes from '../routes/Bills';
+import userRoutes from "../routes/Users";
+import billRoutes from "../routes/Bills";
+import assistsRoutes from "../routes/Assits";
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/users', userRoutes);
-app.use('/users/:id/bills', billRoutes);
+app.use("/users", userRoutes);
+app.use("/users", billRoutes);
+app.use("/users", assistsRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Main Page !");
