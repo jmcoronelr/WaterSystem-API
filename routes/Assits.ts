b060b4a8b3c5b28f3644d41f9db27ac95d60a022
Marcelo Coronel-Linux <jmcoronelr19@gmail.com>
2024-02-05 12:06:03 -0300

    Assist routes added

--- routes/Assits.ts
+++ routes/Assits.ts
@@ -1,14 +1,25 @@
 import { Router, Request, Response } from "express";
-
+import * as controller from "../Controllers/AssistController";
 const router = Router();
-
-//Create new Assist
-
 
 //Get all Assists
 
+router.get("/:id/assists", controller.getAssists);
+
+//Get Assists by id
+
+router.get("/:id/assists/:assistid", controller.getAssistById);
+
+//Create new Assist
+
+router.post("/:id/assists", controller.createAssist);
+
+//Delete Assists
+
+router.delete("/:id/assists/:assistid", controller.deleteAssist);
 
 //Update Assist
 
+router.put("/:id/assists/:assistid", controller.updateAssist);
 
 export default router;
