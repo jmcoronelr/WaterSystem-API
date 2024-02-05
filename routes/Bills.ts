b1f2172f34f0467bed3ddb857b4cd1b4c9f12327
Marcelo Coronel-Linux <jmcoronelr19@gmail.com>
2024-02-05 10:43:26 -0300

    All routes added

--- routes/Bills.ts
+++ routes/Bills.ts
@@ -1,14 +1,25 @@
 import { Router, Request, Response } from "express";
-
+import * as controller from "../Controllers/BillController";
 const router = Router();
-
-//Create new Bill
-
 
 //Get all bills
 
+router.get("/:id/bills", controller.getBills);
+
+//Get bill by id
+
+router.get("/:id/bills/:billid", controller.getBillById);
+
+//Create new Bill
+
+router.post("/:id/bills", controller.createBill);
+
+//Delete Bill
+
+router.delete("/:id/bills/:billid", controller.deleteBill);
 
 //Update Bill
 
+router.put("/:id/bills/:billid", controller.updateBill);
 
 export default router;
